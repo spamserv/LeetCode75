4b10a4111623608279a1567296ffab5086fc3aa5
Josip Vojak <josipvojak@gmail.com>
2023-12-31 12:46:41 +0100

    334. Increasing Triplet Subsequence

--- 238-product-of-array-except-self.js
+++ 238-product-of-array-except-self.js
@@ -44,7 +44,7 @@ const productExceptSelf = function(nums) {
     
     // Then, update the prefix tracker to be the product of itself,
     // multiplied by the input value at the position.
-    for (let i=0; i<nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         result[i] = prefix
         prefix *= nums[i]
     }
@@ -58,7 +58,7 @@ const productExceptSelf = function(nums) {
     
     // Then, update the suffix tracker to be the product of itself,
     // multiplied by the input value at that position.
-    for (let i=nums.length - 1; i>=0; i--) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         result[i] *= suffix
         suffix *= nums[i]
     }
